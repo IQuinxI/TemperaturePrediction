feb3395be2162d4609dd595d36610e260a794fb1
Yassine <aqwzsxcv123@gmail.com>
2022-09-22 21:55:37 +0100

    second push

--- Temperature prediction.py
+++ Temperature prediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn.metrics
 from sklearn.linear_model import LinearRegression
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
@@ -8,7 +9,7 @@ from sklearn.model_selection import train_test_split
 import datetime
 data = pd.read_csv("energydata_complete.csv")
 data = data[["RH_1","T1"]]
-print(data.columns)
+# print(data.columns)
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(data["RH_1"], data["T1"], test_size=0.2, random_state=42)
@@ -31,10 +32,9 @@ def graph():
 
 def VisualDiff(A_P_Data):
     A_P_Data.head(50).plot(kind="bar", figsize=(12,6))
-    plt.show()
+    # plt.show()
 
 def get_current_accuracy():
-    test_x = np.array(X_test[["RH_1"]])
     test_y = np.array(Y_test[["T1"]])
 
     predicted_y = regressor.predict(X_test)
@@ -52,24 +52,24 @@ def graph(X, Y, slope, intercept, Title):
     plt.title(Title)
     plt.xlabel("Humidity")
     plt.ylabel("Temprature")
-    plt.show(
+    # plt.show()
 
-
-    )
 regressor = LinearRegression()
 regressor.fit(X_train, Y_train)
 
 print("intecept: ",regressor.intercept_)
 print("slope: ",regressor.coef_)
 
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X_train)
 
 y_pred = pd.DataFrame(y_pred, columns=["T1"])
 print("Accuracy: ", get_current_accuracy())
-graph(X_train, Y_train, regressor.coef_, regressor.intercept_, "test")
+graph(X_train, Y_train, regressor.coef_, regressor.intercept_, "Figure de regression lineaire")
 
 # print(y_pred)
 # print(Y_test)
-# df = pd.DataFrame({"Actual": Y_test["T1"], "Predicted": y_pred["T1"]})
+Y_train = pd.DataFrame(Y_train).reset_index()
+df = pd.DataFrame({"Actual": Y_train["T1"], "Predicted": y_pred["T1"]})
 # print(df.to_string())
-# VisualDiff(df)
+VisualDiff(df)
+plt.show()
